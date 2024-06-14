f7354ea1248dcb5ffdcf234023367147cd3b107f
Avesh6754 <aweshprajapati150@gmail.com>
2024-06-14 14:05:13 +0530

    14-06-2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-
-void main() {
+void main()
+{
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -11,205 +10,30 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Detailsscreen(),
+      home: First(),
     );
   }
 }
 
-class Detailsscreen extends StatefulWidget {
-  const Detailsscreen({super.key});
+class First extends StatefulWidget {
+  const First({super.key});
 
   @override
-  State<Detailsscreen> createState() => _DetailsscreenState();
+  State<First> createState() => _FirstState();
 }
 
-class _DetailsscreenState extends State<Detailsscreen> {
+class _FirstState extends State<First> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          toolbarHeight: 12,
+    return SafeArea(
+
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Text("Hello\n\n\n\n\n\nDart\n\n\n\n\n\nFlutter",style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold,color: Colors.red,decoration:TextDecoration.underline,decorationColor:Colors.amber),),
         ),
-        body: Column(
-          children: [
-            Expanded(
-              flex: 0,
-              child: Container(
-                width: double.infinity,
-                decoration: BoxDecoration(color: Colors.red),
-                alignment: Alignment.topLeft,
-                child: Padding(
-                  padding: const EdgeInsets.all(13),
-                  child: Icon(Icons.arrow_back),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                width: double.infinity,
-                decoration: BoxDecoration(),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: 12,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 15),
-                          child: Text(
-                            'Airtight Cactus',
-                            style: TextStyle(
-                                fontSize: 25, fontWeight: FontWeight.w500),
-                          ),
-                        ),
-                        Spacer(),
-                        Padding(
-                            padding: EdgeInsets.fromLTRB(0, 10, 12, 0),
-                            child: Icon(Icons.favorite_border))
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(top: 10),
-                          height: 18,
-                          width: 55,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(4),
-                              border: Border.all(
-                                  color: Colors.greenAccent, width: 2),
-                              color: Colors.greenAccent),
-                          alignment: Alignment.center,
-                          child: Text(
-                            '2652.5 Sold',
-                            style: TextStyle(
-                                fontSize: 7, color: Colors.green[900]),
-                          ),
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 10),
-                          child: Icon(
-                            (Icons.star_half_sharp),
-                            color: Colors.green,
-                            size: 20,
-                          ),
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 10),
-                          child: Text(
-                            '4.9(4.747 reviews)',
-                            style: TextStyle(fontSize: 12),
-                          ),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 10),
-                      height: 1,
-                      width: 346,
-                      decoration: BoxDecoration(color: Colors.grey[350]),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 12),
-                      child: Text(
-                        'Description',
-                        style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 12),
-                      child: Text(
-                        'Plants are the eukaryotes that form the kingdom Plant they are predominantly photosynthetic. This means that they obtain their energy from sunlight, using chloroplasts derived from endosymbiosis with using the green pigment chlorophyll.',
-                        style: TextStyle(color: Colors.grey[500]),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(left: 12),
-                          child: Text(
-                            'Quantity',
-                            style: TextStyle(
-                                fontSize: 15, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        SizedBox(
-                          width: 20,
-                        ),
-                        Container(
-                            height: 45,
-                            width: 120,
-                            decoration: BoxDecoration(
-                                color: Colors.grey[200],
-                                borderRadius: BorderRadius.circular(30)),
-                            child: Row(
-                              mainAxisAlignment:
-                                  MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Icon(
-                                  Icons.remove,
-                                  color: Colors.green,
-                                  size: 23,
-                                ),
-                                Text(
-                                  '1',
-                                  style: TextStyle(
-                                      fontSize: 20, color: Colors.green),
-                                ),
-                                Icon(
-                                  Icons.add,
-                                  color: Colors.green,
-                                  size: 23,
-                                ),
-                              ],
-                            ))
-                      ],
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 10),
-                      height: 1,
-                      width: 346,
-                      decoration: BoxDecoration(color: Colors.grey[350]),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ));
+      ),
+    );
   }
 }
-// Padding(
-// padding: EdgeInsets.only(left: 12),
-// child: Text(
 
-// ),
-// )
